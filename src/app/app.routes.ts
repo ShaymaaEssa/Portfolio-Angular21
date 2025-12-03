bdc1ffb630952a45adf478a19d23c155bb2249fd
ShaymaaEssa <flowrathesky@gmail.com>
2025-12-03 11:12:26 +0200

    components and layoutss

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { Pathes } from './core/environment/pathes';
+import { Home } from './pages/home/home';
+import { About } from './pages/about/about';
+import { Skills } from './pages/skills/skills';
+import { Projects } from './pages/projects/projects';
+import { Contact } from './pages/contact/contact';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:Pathes.home, component: Home},
+    {path:Pathes.about, component:About},
+    {path:Pathes.skills, component:Skills},
+    {path:Pathes.projects, component:Projects},
+    {path:Pathes.contact, component:Contact}
+];
---
+++ src/app/core/environment/pathes.ts
@@ -0,0 +1,7 @@
+export const Pathes = {
+    home: '',
+    about: 'about',
+    projects: 'projects',
+    skills: 'skills',
+    contact: 'contact'
+};
